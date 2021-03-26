04b08f47f38192be4c5008afe5744cb9c9ac04d4
Joe Anderson <joe@osbert.me>
2021-03-26 06:30:32 +0000

    Remove assets:precompile from entrypoint

--- docker/entrypoint-web.sh
+++ docker/entrypoint-web.sh
@@ -8,8 +8,4 @@ rm -f /code/tmp/pids/server.pid
 bundle exec rails db:create
 bundle exec rails db:migrate
 
-if [ "$RAILS_ENV" = "production" ]; then
-  bundle exec rails assets:precompile
-fi
-
 exec "$@"
